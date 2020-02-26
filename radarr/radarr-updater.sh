5f814ee9a3d15adf6d1d223dedfedfef8ec856da
connedigital <41178400+connedigital@users.noreply.github.com>
2020-02-26 16:47:05 -0300

    Update radarr-updater.sh

--- radarr/radarr-updater.sh
+++ radarr/radarr-updater.sh
@@ -2,7 +2,7 @@
 # radarr-updater by @connedigital
 
 # Global value
-user="radarr"
+user="mediaserver"
 installdir="/opt/$user"
 
 # working directory
@@ -11,7 +11,7 @@ cd $installdir || exit
 # stop radarr first
 service radarr stop
 
-echo "Atualizando radarr. Por favor aguarde!"
+echo "Updating radarr. Please wait!"
 wget -q "$( wget -qO- https://api.github.com/repos/Radarr/Radarr/releases | grep linux.tar.gz | grep browser_download_url | head -1 | cut -d \" -f 4 )"
 tar -xzf Radarr.develop.*.linux.tar.gz
 rm -f Radarr.develop.*.linux.tar.gz
@@ -20,4 +20,4 @@ chown -R $user:$user Radarr
 # start radarr now
 service radarr start
 
-echo "Atualizacao finalizada."
+echo "Update finished."
